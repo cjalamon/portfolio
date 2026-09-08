9c4da4c1c141dfc45143953e7c2400d44a8e11b3
calam3 <calam3@uic.edu>
2026-09-08 12:10:15 -0500

    new

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header(){
         <h1 className = "header-name"> CJ Alamon</h1>
         <nav className ="header-nav">
             <a href = "#home">Home</a>
-            <a href = "#about">About</a>
+            <a href = "#experience">Experience</a>
             <a href = "#projects">Projects</a>
             <a href = "#contact">Contact</a>
         </nav>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,12 +1,34 @@
 import './hero.css'
 import profilePic from '../assets/profilepic.jpg'
+import {useState, useEffect} from 'react'
 
+
+const taglines = [
+    "3rd Year CS Student @ University of Illinois at Chicago",
+    "Aspiring Full-Stack Developer",
+    "Undergraduate Researcher @ Trailblazer Labs",
+]
 function Hero(){
+    const[index, setIndex]= useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % taglines.length)
+        }, 3000)
+
+        return () => clearInterval(interval)
+    }, [])
     return(
         <section className= "hero">
             <div className = "stars"></div>
-            <h1> Hey, I'm CJ! </h1>
-            <p>CS Student @ University of Illinois at Chicago</p>
+            <div className = "wrap">
+                <div className = "ticker">
+                    <span>3rd Year CS Student @ University of Illinois at Chicago&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>3rd Year CS Student @ University of Illinois at Chicago&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                </div>
+            </div>
+                <h1> Hey, I'm CJ! </h1>
+                <p key = {index} className ="tagline">{taglines[index]}</p>
         </section>
     )
 }
